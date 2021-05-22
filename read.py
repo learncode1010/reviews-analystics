b63ff511240dde3ea9c4e83861977961a8e16a75
Brian Su <ccsu1010@gmail.com>
2021-05-22 16:07:17 +0800

    version 1.1

--- read.py
+++ read.py
@@ -1,11 +1,12 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
+	print('開始讀入資料...')
 	for line in f:
 		data.append(line)
 		count = count +1
 		if count % 100000 == 0:
-			print('目前讀入筆數: ', len(data))
-print('總共有', len(data), '筆資料')
+			print( len(data))
+print('總共讀入', len(data), '筆資料')
 
 
